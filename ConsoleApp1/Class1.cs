e72f69ad24b192a04cdc122467cc67e2e171690a
Александр Прилуцкий <ififirfifir@gmail.com>
2021-09-21 20:55:18 +0300

    class1

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Person
+    {
+        public string name = "Ваня";
+        public int age = 18;
+
+        public void GetInfo()
+        {
+            Console.WriteLine("Имя = " + name + "\nВозраст = " + age.ToString());
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Person ivan = new Person();
+            ivan.GetInfo();
+            Console.ReadKey();
+
+            ivan.name = "Андрей";
+            ivan.age = 21;
+            ivan.GetInfo();
+            Console.ReadKey();
+        }
+    }
+}
